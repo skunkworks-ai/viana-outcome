e03cbf4446702b82734730562fe8a92f2ff9245c
eric@meldcx.com <84124191+eric-meldcx@users.noreply.github.com>
2025-05-29 15:06:25 +0800

    Attempt at fixing the CVAT server custom image CLBO workers

--- cvat/settings/outcome.py
+++ cvat/settings/outcome.py
@@ -1,12 +1,13 @@
 import os
 
 # Inherit parent config
-from cvat.settings.production import *  # pylint: disable=wildcard-import
+from .production import *  # pylint: disable=wildcard-import
 
 # Email backend settings for Django
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', None)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '0'))
+if EMAIL_PORT == 587:
+    EMAIL_USE_TLS = True
