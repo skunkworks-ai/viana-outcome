734f7ea8c45a0cedcc51812e017f86c8fd145711
eric@meldcx.com <84124191+eric-meldcx@users.noreply.github.com>
2025-05-30 17:53:29 +0800

    Updated the Outcome Compose and settings files
    - The Compose file sets DJANGO_SETTINGS_MODULE to cvat.settings.outcome
    - The settings file sets the email backend to output to console

--- cvat/settings/outcome.py
+++ cvat/settings/outcome.py
@@ -4,10 +4,4 @@ import os
 from .production import *  # pylint: disable=wildcard-import
 
 # Email backend settings for Django
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ.get('EMAIL_HOST', None)
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '0'))
-if EMAIL_PORT == 587:
-    EMAIL_USE_TLS = True
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
