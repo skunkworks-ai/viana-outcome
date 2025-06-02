e437a4ab0f94e3a32c81d8571fb4940f791bcd16
eric@meldcx.com <84124191+eric-meldcx@users.noreply.github.com>
2025-06-02 13:59:45 +0800

    Updated CVAT references to Outcome in email
    - Changed the CVAT references in the email template
    - Updated the README with information on email environment variables
    - Updated the Outcome Docker Compose and .env.sample for email env vars

--- cvat/settings/outcome.py
+++ cvat/settings/outcome.py
@@ -4,4 +4,12 @@ import os
 from .production import *  # pylint: disable=wildcard-import
 
 # Email backend settings for Django
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST", None)
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", None)
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", None)
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# https://github.com/pennersr/django-allauth
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", None)
